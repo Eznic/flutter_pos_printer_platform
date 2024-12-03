f542254b0cdf0a6893e6fabf113c0e5157304d3b
Eznic <riyan.eznic@gmail.com>
2024-12-03 14:34:07 +0700

    Fix Nullable And Downgrade Rxdart Version

--- lib/src/connectors/bluetooth.dart
+++ lib/src/connectors/bluetooth.dart
@@ -166,7 +166,7 @@ class BluetoothPrinterConnector implements PrinterConnector<BluetoothPrinterInpu
   bool _addDevice(PrinterDevice device) {
     bool isDeviceAdded = true;
     final list = _scanResults.value;
-    if (!list.any((e) => e.address == device.address))
+    if (!list!.any((e) => e.address == device.address))
       list.add(device);
     else
       isDeviceAdded = false;
